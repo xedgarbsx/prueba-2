ee49404968e52a10428a2a209fc29f929d6de6dd
paulmalpartida <pmalpartidar@soen.pe>
2022-12-12 11:23:32 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+'use strict'
+const GI = {
+	createElement (tag, atributes) {
+		let element = document
+		return document.createElement(tag)
+	}
+}
+
+
+const to_do = [
+    'Alimentar al perro',
+    'Estudiar para prueba escrita',
+    'Pagar taxes',
+    'Pasear a perro',
+    'Comprar entradas para el cine',
+]
+
